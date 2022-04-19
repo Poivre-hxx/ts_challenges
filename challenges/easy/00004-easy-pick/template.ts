4fe2980859b392a96165e0e753300ccfe9415b78
poivre <poivre0101@gmail.com>
2022-04-19 23:44:46 +0800

    add: easy-pick
    开始焦虑了

---
+++ challenges/easy/00004-easy-pick/template.ts
@@ -0,0 +1,18 @@
+type MyPick<T, K extends keyof T> = {
+  [U in K]: T[U]; //[a in b] 遍历
+};
+
+// js
+function MyPick(todo, keys) {
+  const obj = {};
+  keys.forEach((key) => {
+    console.log(key);
+    if (key in todo) obj[key] = todo[key];
+  });
+
+  return obj;
+}
+// foreach => mapped
+// todo[key] => indexed
+// keyof => lookup
+// extends => Generic Constraints
