72cbd75ed48e80dbefb90e430a0b4578a0e5cba7
poivre <poivre0101@gmail.com>
2022-04-19 22:39:11 +0800

    add: warm_up

---
+++ challenges/warm-up/hello_world/template.ts
@@ -0,0 +1,2 @@
+// type HelloWorld = string | number // expected to be a string
+type HelloWorld = string // expected to be a string
---
+++ challenges/warm-up/hello_world/test-cases.ts
@@ -0,0 +1,6 @@
+import { Equal, Expect, NotAny } from '@type-challenges/utils'
+
+type cases = [
+  Expect<NotAny<HelloWorld>>, //HelloWorld 包含类型
+  Expect<Equal<HelloWorld, string>> //HelloWorld 与 string 的类型完全相同
+]
