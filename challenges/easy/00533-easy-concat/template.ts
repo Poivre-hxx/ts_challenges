e3c98bc7bfb3df6959155108ae5a77cfd33da4f4
poivre <poivre0101@gmail.com>
2022-04-29 12:08:45 +0800

    add: 00533-easy-concat
    三点运算符。。。。

---
+++ challenges/easy/00533-easy-concat/template.ts
@@ -0,0 +1,25 @@
+type Concat<T extends any[], U extends any[]> = [...T, ...U];
+type t1 = Concat<[1], []>;
+type t2 = Concat<[], [1]>;
+type t3 = Concat<['1', 2, '3'], [false, boolean, '4']>;
+
+
+// js
+function Concat(T, U) {
+  const obj = [];
+  let i;
+  for (i = 0; i < T.length; i++) {
+    obj[i] = T[i];
+  }
+  let j;
+  for (j = 0; j < U.length; j++) {
+      obj[j + i] = U[j];
+  }
+  return obj;
+}
+const arr1 = [1, 2];
+const arr2 = [];
+let t = Concat(arr1, arr2);
+
+// 解构
+// ...运算符
