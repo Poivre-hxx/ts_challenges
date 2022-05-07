027c4e1fa4fbd3c1be3fe09416374a5d5ef5c9aa
poivre <poivre0101@gmail.com>
2022-05-08 01:25:40 +0800

    add: 00533-easy-concat
    学习后的改进版本

--- challenges/easy/00533-easy-concat/template.ts
+++ challenges/easy/00533-easy-concat/template.ts
@@ -1,8 +1,7 @@
-type Concat<T extends any[], U extends any[]> = [...T, ...U];
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U];
 type t1 = Concat<[1], []>;
-type t2 = Concat<[], [1]>;
-type t3 = Concat<['1', 2, '3'], [false, boolean, '4']>;
-
+//type t2 = Concat<[], [1]>;
+type t3 = Concat<["1", 2, "3"], [false, boolean, "4"]>;
 
 // js
 function Concat(T, U) {
@@ -13,13 +12,19 @@ function Concat(T, U) {
   }
   let j;
   for (j = 0; j < U.length; j++) {
-      obj[j + i] = U[j];
+    obj[j + i] = U[j];
   }
   return obj;
 }
 const arr1 = [1, 2];
 const arr2 = [];
 let t = Concat(arr1, arr2);
+
+function Concat1(arr1, arr2) {
+  // spread 操作符
+
+  return [...arr1, ...arr2];
+}
 
 // 解构
 // ...运算符
