384619d124368c02757324635b73f032abc876b1
poivre <poivre0101@gmail.com>
2022-05-13 00:39:35 +0800

    fix: 00898-easy-includes
    加入了一些芝士点

--- challenges/easy/00898-easy-includes/template.ts
+++ challenges/easy/00898-easy-includes/template.ts
@@ -1,24 +1,22 @@
-import type{ Equal } from '@type-challenges/utils'
+import type { Equal } from "@type-challenges/utils";
 
+// false
 // type Includes<T extends any[], K> = T["length"] extends 0
-// ? false  
-// : K extends T[number] 
-//     ? T[number] extends K ? true : false 
+// ? false
+// : K extends T[number]
+//     ? T[number] extends K ? true : false
 //     : false;
 
 type Includes<T extends any[], K> = T extends [infer X, ...infer Y]
-? Equal<X, K> extends true ? true : Includes<Y, K>
-: false; 
+  ? Equal<X, K> extends true
+    ? true
+    : Includes<Y, K>
+  : false;
 
-export {Includes} 
-
-type q1 = Includes<["Kars", "Esidisi", "Wamuu", "Santana"], "Kars">;
-type q2 = Includes<['Kars', 'Esidisi', 'Wamuu', 'Santana'], 'Dio'>;
-type q3 = Includes<[{}], { a: 'A' }>;
-type q4 = Includes<[1 | 2], 1>;
-
+export { Includes };
 
 // js
+// 普通的实现方式
 // function Includes(T, U) {
 //     for(let i = 0; i < T.length; i++){
 //         if(T[i] === U)
@@ -26,5 +24,23 @@ type q4 = Includes<[1 | 2], 1>;
 //     }
 //     return false;
 // }
-// let t = Includes(["Kars", "Esidisi", "Wamuu", "Santana"], "Kars");
+// 递归实现方式
+function Includes(T, U) {
+  function _(T, U) {
+    if (T.length === 0) return false;
+    const [first, ...rest] = T;
 
+    if (first === U) {
+      return true;
+    } else {
+      return _(rest, U);
+    }
+  }
+  return _(T, U);
+}
+
+// 1. ts 遍历的方式
+// {[P in keyod T]: P} 得到的是一个对象
+// 2. ts 的模块规范
+//    如果有 export/import 的话，那么就是模块er
+//    没有的话，那么就是全局的，可以直接在别的模块er引用 
