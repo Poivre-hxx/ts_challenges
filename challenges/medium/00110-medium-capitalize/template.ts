b769d85b2a8a2b7bedf8e487871aee053830404d
poivre <poivre0101@gmail.com>
2022-05-17 11:35:49 +0800

    fix: 00110-medium-capitalize
    有意思的实现方法

--- challenges/medium/00110-medium-capitalize/template.ts
+++ challenges/medium/00110-medium-capitalize/template.ts
@@ -1,12 +1,51 @@
-type MyCapitalize<S extends string> = S extends `${infer T}${infer R}` 
-    ? `${Uppercase<T>}${R}` 
-    : S;
-type t1 = MyCapitalize<'foobar'>;
+// 实现方法一
+// type MyCapitalize<S extends string> = S extends `${infer T}${infer R}`
+//     ? `${Uppercase<T>}${R}`
+//     : S;
 
-// js 
+// 实现方法二
+type Letter = {
+  a: "A";
+  b: "B";
+  c: "C";
+  d: "D";
+  e: "E";
+  f: "F";
+  g: "G";
+  h: "H";
+  i: "I";
+  j: "J";
+  k: "K";
+  l: "L";
+  m: "M";
+  n: "N";
+  o: "O";
+  p: "P";
+  q: "Q";
+  r: "R";
+  s: "S";
+  t: "T";
+  u: "U";
+  v: "V";
+  w: "W";
+  x: "X";
+  y: "Y";
+  z: "Z";
+};
+
+type MyCapitalize<S extends string> = S extends `${infer F}${infer L}`
+  ? F extends keyof Letter
+    ? `${Letter[F]}${L}`
+    : S
+  : S;
+
+type t1 = MyCapitalize<"foobar">;
+
+// js
 function MyCapitalize(str) {
-    str[0].toUpperCase();
-    return str;
+  str[0].toUpperCase();
+  return str;
 }
 
 // `${}`  ES6 的字符串语法
+// keyof 遍历
