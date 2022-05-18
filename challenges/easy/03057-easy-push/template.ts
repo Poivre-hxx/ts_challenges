e1738826f90d1d484bee93a1c976b2008adb071a
poivre <poivre0101@gmail.com>
2022-05-19 01:03:29 +0800

    fix: update easy-push & easy-unshift

--- challenges/easy/03057-easy-push/template.ts
+++ challenges/easy/03057-easy-push/template.ts
@@ -1,11 +1,16 @@
-type Push<T extends any[], U> = [...T, U];
+type Push<T extends unknown[], U> = [...T, U];
 
 // js 
-function Push(T, U)
-{
-    //const obj = [];
-    let i = T.length;
-    T[i] = U.value;
-    return T;
+// function Push(T, U)
+// {
+//     //const obj = [];
+//     let i = T.length;
+//     T[i] = U.value;
+//     return T;
+// }
+function Push(T, U) {
+    return [...T, U];
 }
 let t1 = Push([1, 2], [3]);
+
+// spread 操作符
--- challenges/easy/03060-easy-unshift/template.ts
+++ challenges/easy/03060-easy-unshift/template.ts
@@ -1,11 +1,17 @@
-type Unshift<T extends any[], U> = [U, ...T];
+type Unshift<T extends unknown[], U> = [U, ...T];
 
 // js 
-function Unshift(T, U){
-    const obj = [];
-    let i = T.length;
-    obj[0] = U[0]; 
-    for(let j = 1; j <= i; j++) obj[j] = T[j-1];
-    return obj;
+// function Unshift(T, U){
+//     const obj = [];
+//     let i = T.length;
+//     obj[0] = U[0]; 
+//     for(let j = 1; j <= i; j++) obj[j] = T[j-1];
+//     return obj;
+// }
+function Unshift(T, U) {
+    return [U, ...T];
 }
 let t1 = Unshift([1, 2],'3');
+
+
+// spread 操作符
