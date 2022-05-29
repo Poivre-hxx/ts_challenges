868c8fe103cd8f34e426f1b4daeff703aced2c4d
poivre <poivre0101@gmail.com>
2022-05-29 13:35:53 +0800

    00949-medium-anyof

---
+++ challenges/medium/00949-medium-anyof/template.ts
@@ -0,0 +1,8 @@
+type AnyOf<T extends readonly any[]> = T extends [infer F, ...infer R]
+  ? F extends [] | Record<string, never> | '' | 0 | false 
+      ? AnyOf<R>
+      : true
+  : false;
+type t1 = AnyOf<[1, "", false, [], {}]>;
+
+// Record 类型 => 解构对象
